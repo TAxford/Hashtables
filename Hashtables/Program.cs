9f5589fbf77173cf51c5d7135ad0eb762a2d2208
Timothy Axford <t.axford2@yahoo.com>
2023-03-29 10:46:05 -0400

    Added a Student class

--- Hashtables/Program.cs
+++ Hashtables/Program.cs
@@ -3,10 +3,30 @@
 namespace Hashtables
 {
     internal class Program
-    {
+    {   
+        // Key - Value
+        // Auto - Car
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Student
+    {
+        //property called id
+        public int Id { get; set; }
+        //property called Name
+        public string Name { get; set; }
+        //property called GPA
+        public float GPA { get; set; }
+        //simple constructor
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
         }
     }
 }
